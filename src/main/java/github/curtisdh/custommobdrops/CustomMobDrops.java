31de38dfd8b0717c202a4bd4051359679eff6c0f
CurtisDH <39945904+CurtisDH@users.noreply.github.com>
2021-09-26 02:22:36 +1000

    - Created a config to specify the mob, dropRate and item to be dropped

--- src/main/java/github/curtisdh/custommobdrops/CustomMobDrops.java
+++ src/main/java/github/curtisdh/custommobdrops/CustomMobDrops.java
@@ -1,14 +1,76 @@
 package github.curtisdh.custommobdrops;
 
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public final class CustomMobDrops extends JavaPlugin
 {
-
+    onEntityDeathEvent deathEvent;
     @Override
     public void onEnable()
     {
-        getServer().getPluginManager().registerEvents(new onEntityDeathEvent(), this);
+        PrintWithClassName(this,"Starting...");
+        deathEvent = new onEntityDeathEvent();
+        LoadConfig();
+        getServer().getPluginManager().registerEvents(deathEvent, this);
+        PrintWithClassName(this,"Initialised");
+    }
+    public static void PrintWithClassName(Object obj,String str)
+    {
+        System.out.println(obj.getClass().getName()+"::"+str);
+    }
+    private void LoadConfig()
+    {
+        PrintWithClassName(this,"Loading config");
+        saveDefaultConfig();
+        Map<String, Map<String, Integer>> EntityData = new HashMap<>();
+        for (String key : getConfig().getConfigurationSection("settings.mobs").getKeys(false))
+        {
+            Map<String,Integer> mobDropInfo = new HashMap<>();
+            Object droprateObj = getConfig().getConfigurationSection("settings.mobs." + key)
+                    .getValues(true).get("droprate");
+
+            Object itemMaterial = getConfig().getConfigurationSection("settings.mobs." + key)
+                    .getValues(true).get("itemMaterial");
+
+            int dropRate = Integer.parseInt(droprateObj.toString());
+            key = key.toUpperCase(Locale.ROOT);
+
+            mobDropInfo.put(itemMaterial.toString(),dropRate);
+            PrintWithClassName(this,"Loaded mob: "+key+
+                    "materialDrop: "+ itemMaterial + " droprate:"+dropRate);
+            EntityData.put(key, mobDropInfo);
+        }
+        deathEvent.EntityItemToDropAndDropRate = EntityData;
+        //TODO access the saved entity data, get the drop rate and item material and add it to the hashmap
+//        int dropRate;
+//        try
+//        {
+//            dropRate = Integer.parseInt(getConfig().getString("settings.mobs.husk.droprate"));
+//        }
+//        catch (Exception e)
+//        {
+//            PrintWithClassName(this,"droprate in config is undefined. Setting droprate to 1");
+//            PrintWithClassName(this,"Config location is:"+getDataFolder().getAbsolutePath());
+//            getConfig().createSection("settings.mobs.husk.droprate");
+//            getConfig().set("settings.mobs.husk.droprate",1);
+//            dropRate = 1;
+//        }
+//        String itemToDrop = getConfig().getString("settings.mobs.husk.itemMaterial");
+//        if(itemToDrop == null)
+//        {
+//            PrintWithClassName(this,"itemToDrop is undefined. Setting to 'SAND'");
+//            getConfig().createSection("settings.mobs.husk.itemMaterial");
+//            PrintWithClassName(this,"Config location is:"+getDataFolder().getAbsolutePath());
+//            getConfig().set("settings.mobs.husk.itemMaterial","SAND");
+//        }
+        saveConfig();
     }
 
     @Override
--- src/main/java/github/curtisdh/custommobdrops/onEntityDeathEvent.java
+++ src/main/java/github/curtisdh/custommobdrops/onEntityDeathEvent.java
@@ -1,6 +1,8 @@
 package github.curtisdh.custommobdrops;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Husk;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
@@ -8,15 +10,44 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Map;
+
 public class onEntityDeathEvent implements Listener
 {
+    public Map<String, Map<String, Integer>> EntityItemToDropAndDropRate;
+
     @EventHandler
     public void mobDeathEvent(EntityDeathEvent event)
     {
         LivingEntity entity = event.getEntity();
-        if(entity instanceof Husk)
+        for (Map.Entry<String, Map<String, Integer>> entities : EntityItemToDropAndDropRate.entrySet())
         {
-            entity.getLocation().getWorld().dropItemNaturally(entity.getLocation(),new ItemStack(Material.SAND,1));
+            try
+            {
+                EntityType.valueOf(entities.getKey());
+            }
+            catch(Exception e)
+            {
+                System.out.println("NO ENTITY FOUND OF TYPE:"+entities.getKey());
+                System.out.println(e);
+                return;
+            }
+
+            if (entity.getType() == EntityType.valueOf(entities.getKey()))
+            {
+                for (Map.Entry<String, Integer> ItemMaterialAndDropRate : entities.getValue().entrySet())
+                {
+                    entity.getLocation().getWorld().dropItemNaturally(entity.getLocation(),
+                            new ItemStack(
+                                    Material.valueOf(ItemMaterialAndDropRate.getKey()),
+                                    ItemMaterialAndDropRate.getValue()));
+
+                }
+            }
         }
+//        if(entity instanceof Husk)
+//        {
+//            entity.getLocation().getWorld().dropItemNaturally(entity.getLocation(),new ItemStack(Material.valueOf(itemMaterial), dropRate));
+//        }
     }
 }
